405657beccc86335fa5e0a22556bd2a5cf31cb55
LouisPerry4281 <louisperry4281@gmail.com>
2024-03-24 14:20:18 +0000

    Added Hitstop & Knockback
    Added hitstop and basic knockback when the enemy is hit, though currently the enemy is just floating away, will fix later.

--- Inertia/Assets/Scripts/EnemyHealth.cs
+++ Inertia/Assets/Scripts/EnemyHealth.cs
@@ -2,19 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] float health;
+    [SerializeField] float knockbackForce;
     bool isVulnerable = true;
 
     [SerializeField] GameObject hitEffect;
 
+    Rigidbody rb;
     GameManager gameManager;
+    Animator playerAnimator;
+    NavMeshAgent agent;
 
     private void Start()
     {
         gameManager = FindAnyObjectByType<GameManager>();
+        playerAnimator = GameObject.Find("Neutral Idle").GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     public void InitialiseDamage(float damageToTake, float damageTimer)
@@ -27,11 +35,29 @@ public class EnemyHealth : MonoBehaviour
     {
         isVulnerable = false;
 
+        //Hitstop Stuff
+        playerAnimator.speed = 0;
+        playerAnimator.GetComponentInParent<Rigidbody>().velocity = Vector3.zero;
+        yield return new WaitForSeconds(0.2f);
+        playerAnimator.speed = 1;
+
+        //agent.updatePosition = false;
+        //agent.velocity = Vector3.zero;
+        //agent.ResetPath();
+
+        rb.isKinematic = false;
+        agent.enabled = false;
+
+        Knockback();
+
+        //yield return new WaitForSeconds(0.5f);
+
+        //agent.enabled = true;
+
         health -= damageToTake;
         if (HealthCheck())
             yield return null;
 
-        print("hit");
         Instantiate(hitEffect, new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z), Quaternion.identity);
 
         yield return new WaitForSeconds(damageTimer);
@@ -48,5 +74,12 @@ public class EnemyHealth : MonoBehaviour
         }
 
         return false;
+    }
+
+    private void Knockback()
+    {
+        print("YEET");  
+        Vector3 direction = (playerAnimator.transform.position - transform.position).normalized;
+        rb.AddForce(-direction * knockbackForce, ForceMode.Impulse);
     }
 }
