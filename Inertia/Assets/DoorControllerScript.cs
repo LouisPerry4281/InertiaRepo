cc312c1e0aeeb2377921f1f1ae38bd7fd9cae396
LouisPerry4281 <louisperry4281@gmail.com>
2024-03-31 13:16:13 +0100

    Door Controller Scripts

---
+++ Inertia/Assets/DoorControllerScript.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControllerScript : MonoBehaviour
+{
+    Animator animator;
+
+    // 0 - Front Door
+    // 1 - Generic Spawn Door
+    // 2 - Exit Door
+    [SerializeField] int doorType;
+
+    bool doorReadyToOpen = false;
+    bool doorReadyToClose = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        switch(doorType)
+        {
+            case 0:
+                FrontDoorLogic();
+                break;
+            case 1:
+                SpawnDoorLogic();
+                break;
+            case 2:
+                ExitDoorLogic();
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        doorReadyToOpen = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        doorReadyToClose = true;
+    }
+
+    private void FrontDoorLogic()
+    {
+        if (doorReadyToOpen)
+        {
+            animator.SetTrigger("OpenDoor");
+            SequenceController.IncrementSequence();
+            
+        }
+    }
+
+    private void SpawnDoorLogic()
+    {
+
+    }
+
+    private void ExitDoorLogic()
+    {
+
+    }
+}
---
+++ Inertia/Assets/SequenceController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceController : MonoBehaviour
+{
+    // 0 - Before Combat
+    // 1 - During Combat
+    // 2 - Close To Done
+    // 3 - Exit Unlocked
+    public static int sequenceIndex = 0;
+
+    [SerializeField] GameObject frontDoor;
+
+    [SerializeField] GameObject bay1Door;
+    [SerializeField] GameObject bay2Door;
+    [SerializeField] GameObject bay3Door;
+
+    public static void IncrementSequence()
+    {
+        sequenceIndex++;
+    }
+}
