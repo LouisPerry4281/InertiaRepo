58efd57616bf6a06c0537eb9966ee49fb23ef72c
LouisPerry4281 <louisperry4281@gmail.com>
2024-04-20 14:50:59 +0100

    Revert "Imported GOAP Core"
    This reverts commit 32b029e3fd0590203e35c36600671e264c31fb9d.

---
+++ Inertia/Assets/SequenceController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceController : MonoBehaviour
+{
+    // 0 - Before Combat
+    // 1 - During Combat
+    // 2 - Close To Done
+    // 3 - Exit Unlocked
+    public static int sequenceIndex = 0;
+
+    GameObject endTrigger;
+
+    [SerializeField] GameObject frontDoor;
+
+    [SerializeField] GameObject bay1Door;
+    [SerializeField] GameObject bay2Door;
+    [SerializeField] GameObject bay3Door;
+
+    private void Start()
+    {
+        endTrigger = FindAnyObjectByType<EndTrigger>().gameObject;
+        endTrigger.SetActive(false);
+    }
+
+    public void IncrementSequence()
+    {
+        sequenceIndex++;
+
+        if (sequenceIndex >= 4)
+        {
+            endTrigger.SetActive(true);
+        }
+
+        Invoke(nameof(StartCombat), 2f);
+    }
+
+    void StartCombat()
+    {
+        GameManager.hasStartedCombat = true;
+    }
+}
---
+++ Inertia/Assets/WaveController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+{
+    [SerializeField] Transform[] spawnLocations;
+
+    [SerializeField] GameObject enemy;
+
+    [SerializeField] int[] enemiesToSpawn;
+
+    GameManager gameManager;
+    SequenceController sc;
+
+    int enemiesLeftToSpawn;
+    public int currentWave = 0;
+    
+    float spawnIntervalTimer;
+    [SerializeField] float spawnInterval;
+
+    private void Start()
+    {
+        sc = FindAnyObjectByType<SequenceController>();
+
+        enemiesLeftToSpawn = enemiesToSpawn[currentWave];
+    }
+
+    private void Update()
+    {
+        spawnIntervalTimer -= Time.deltaTime;
+
+        if (enemiesLeftToSpawn > 0 && spawnIntervalTimer <= 0)
+            SpawnEnemy();
+
+        if (enemiesLeftToSpawn <= 0 && GameManager.enemyCount <= 2 && currentWave <= 1 && GameManager.hasStartedCombat)
+            NextWave();
+
+        else if (currentWave == 2 && enemiesLeftToSpawn <= 0 && GameManager.enemyCount <= 0 && GameManager.hasStartedCombat)
+        {
+            NextWave();
+        }
+    }
+
+    public void SpawnEnemy()
+    {
+        int locationToSpawn = Random.Range(0, spawnLocations.Length);
+        Instantiate(enemy, spawnLocations[locationToSpawn]);
+
+        enemiesLeftToSpawn--;
+
+        GameManager.enemyCount++;
+
+        spawnIntervalTimer = spawnInterval;
+    }
+
+    public void NextWave()
+    {
+        currentWave++;
+
+        if (currentWave <= 2 && GameManager.enemyCount <= 5)
+            sc.IncrementSequence();
+
+        if (currentWave >= 3)
+        {
+            sc.IncrementSequence();
+            Destroy(this);
+        }
+
+        enemiesLeftToSpawn = enemiesToSpawn[currentWave];
+    }
+}
