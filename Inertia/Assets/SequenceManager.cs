ec73282f51e715d7bbd73cdc8f42a5a27fa31fe9
Invernness <104453132+Invernness@users.noreply.github.com>
2024-05-13 19:56:25 +0100

    Factory Ambience
    Imported the factory ambience, but waiting on the updating of the Audio Manager before this is finally setup, as currently the audio manager doesn't support multiple sounds.

--- Inertia/Assets/SequenceManager.cs
+++ Inertia/Assets/SequenceManager.cs
@@ -105,6 +105,7 @@ public class SequenceManager : MonoBehaviour
 
         AudioManager.instance.PlaySFX("IntruderAlert", 1, 1);
         AudioManager.instance.PlayMusic("Glow");
+        //AudioManager.instance.PlayMusic("FactoryNoise"); Not currently active, as the audio manager doesnt support multiple sounds yet.
         Invoke("Siren", 1f);
     }
 
