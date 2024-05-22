6df4860ef49454d82ae81606654cec747a4da2fe
LouisPerry4281 <louisperry4281@gmail.com>
2024-05-22 11:07:26 +0100

    Pause Menu Functionality

---
+++ Inertia/Assets/PauseMenuManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject settingsMenu;
+
+    bool isPaused;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                Pause();
+            }
+
+            else if (isPaused)
+            {
+                CloseMenus();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+
+        settingsMenu.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+
+    public void Settings()
+    {
+        pauseMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void CloseMenus()
+    {
+        Time.timeScale = 1;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        pauseMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        //Loads the main menu
+        SceneManager.LoadScene(0);
+    }
+}
