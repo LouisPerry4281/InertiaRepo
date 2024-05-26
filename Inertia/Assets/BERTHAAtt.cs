a961a8c592ea6e982b30cf0a2eb620e5ed22b588
LouisPerry4281 <louisperry4281@gmail.com>
2024-05-26 22:45:35 +0100

    BERTHA Polish
    Added some particles and camera shake.

---
+++ Inertia/Assets/BERTHAAtt.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BERTHAAtt : MonoBehaviour
+{
+    public float cameraShakeMagnitude;
+    public float cameraShakeLength;
+
+    public Transform floorTarget1;
+    public Transform floorTarget2;
+
+    public GameObject rubble;
+
+    bool isSecondHit;
+
+    public void HitGround()
+    {
+        CinemachineShake.Instance.ShakeCamera(cameraShakeMagnitude, cameraShakeLength);
+
+        if (isSecondHit)
+        {
+            GameObject rubbleInstance = Instantiate(rubble, floorTarget2.position, Quaternion.identity);
+            rubbleInstance.transform.eulerAngles = new Vector3(-90, 0, 0);
+            isSecondHit = false;
+        }
+
+        else
+        {
+            GameObject rubbleInstance = Instantiate(rubble, floorTarget1.position, Quaternion.identity);
+            rubbleInstance.transform.eulerAngles = new Vector3(-90, 0, 0);
+            isSecondHit = true;
+        }
+    }
+}
